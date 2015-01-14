20d62b5d264c7c08d174d7d62936329feabc3e10
Olaf Thielke <olaf@coachseek.com>
2015-01-15 09:08:09 +1300

    Enable Cross Origin Resource Sharing.

--- CoachSeek.Api/App_Start/WebApiConfig.cs
+++ CoachSeek.Api/App_Start/WebApiConfig.cs
@@ -1,4 +1,5 @@
 ﻿using System.Web.Http;
+using System.Web.Http.Cors;
 using System.Web.Http.Dispatcher;
 using CoachSeek.Application.Configuration;
 using CoachSeek.DataAccess.Configuration;
@@ -12,6 +13,8 @@ namespace CoachSeek.Api
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            var cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
 
             WebApiAutoMapperConfigurator.Configure();
             ApplicationAutoMapperConfigurator.Configure();
