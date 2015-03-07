548a348b33d5cde2defe450b840f663599b80dfc
Olaf Thielke <olaf@coachseek.com>
2015-03-07 15:05:45 +1300

    Fix location, coach and service not updating with session.

--- CoachSeek.Api/Attributes/TestingFilter.cs
+++ CoachSeek.Api/Attributes/TestingFilter.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Remoting.Messaging;
-using System.Web;
-using System.Web.Http.Controllers;
+﻿using System.Web.Http.Controllers;
 using System.Web.Http.Filters;
 using CoachSeek.Api.Controllers;
 using CoachSeek.DataAccess.Main.Memory.Repositories;
