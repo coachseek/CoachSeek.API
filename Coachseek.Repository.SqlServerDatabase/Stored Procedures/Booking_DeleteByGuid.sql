6f348e37c490e41f6a17e323e536cfb4946c8d21
Olaf Thielke <olaf@coachseek.com>
2015-04-10 19:07:56 +1200

    Deleting course booking also deletes associated session bookings.

--- Coachseek.Repository.SqlServerDatabase/Stored Procedures/Booking_DeleteByGuid.sql
+++ Coachseek.Repository.SqlServerDatabase/Stored Procedures/Booking_DeleteByGuid.sql
@@ -5,16 +5,35 @@ CREATE PROCEDURE [dbo].[Booking_DeleteByGuid]
 AS
 BEGIN
 	SET NOCOUNT ON;
+
+	WITH CourseBooking AS 
+	( 
+		SELECT 
+			bk.[Id], 
+			bk.[ParentId]
+		FROM 
+			[dbo].[Booking] bk
+			INNER JOIN [dbo].[Business] b
+				ON b.[Id] = bk.[BusinessId] 
+		WHERE
+			bk.[Guid] = @bookingGuid
+			AND b.[Guid] = @businessGuid
+		UNION ALL 
+		SELECT
+			bk2.[Id], 
+			bk2.[ParentId]
+		FROM
+			[Booking] bk2
+			INNER JOIN CourseBooking 
+				ON CourseBooking.[Id] = bk2.[ParentId] 
+	)
 	
 	DELETE 
-		bk 
+		bk
 	FROM 
-		[dbo].[Business] b
-		INNER JOIN [dbo].[Booking] bk
-			ON b.[Id] = bk.[BusinessId]
-	WHERE
-		b.[Guid] = @businessGuid
-		AND bk.[Guid] = @bookingGuid
-
+		[dbo].[Booking] bk
+		INNER JOIN CourseBooking cb
+			ON bk.[Id] = cb.[Id]
+	
 END
 
