dfd4477cfd50660c18028bf03df06fff3cae75e4
Olaf Thielke <olaf@coachseek.com>
2015-05-25 22:02:19 +1200

    Default currency to NZD.

--- CoachSeek.Domain/Entities/Currency.cs
+++ CoachSeek.Domain/Entities/Currency.cs
@@ -1,5 +1,4 @@
-﻿using System.Xml.Schema;
-using CoachSeek.Common.Extensions;
+﻿using CoachSeek.Common.Extensions;
 using CoachSeek.Domain.Exceptions;
 using CoachSeek.Domain.Repositories;
 
@@ -7,11 +6,15 @@ namespace CoachSeek.Domain.Entities
 {
     public class Currency
     {
+        private const string DEFAULT_CURRENCY = "NZD";
+
         public string Code { get; private set; }
         public string Symbol { get; private set; }
 
         public Currency(string code, ISupportedCurrencyRepository supportedCurrencyRepository)
         {
+            if (string.IsNullOrEmpty(code))
+                code = DEFAULT_CURRENCY;
             var currency = supportedCurrencyRepository.GetByCode(code);
             if (currency.IsNotFound())
                 throw new CurrencyNotSupported();
