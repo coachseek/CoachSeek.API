7ffb054d6bd4ceab022beb9058f5f94a9976d679
Olaf Thielke <olaf@coachseek.com>
2015-05-31 18:53:57 +1200

    Add PayPal testing project.

--- Coachseek.Integration.Payments.Paypal.Tester/Controllers/HomeController.cs
+++ Coachseek.Integration.Payments.Paypal.Tester/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -25,6 +27,30 @@ namespace Coachseek.Integration.Payments.Paypal.Tester.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult Payment()
+        {
+            ViewBag.Message = "Your payment page.";
+
+            return View();
+        }
+
+        public RedirectResult MakePayment()
+        {
+            ViewBag.Message = "Your payment page.";
+
+            var paypalParameters = new StringBuilder();
+            paypalParameters.AppendFormat("business={0}", "olaf-facilitator@coachseek.com");
+            paypalParameters.AppendFormat("&currency_code={0}", "NZD");
+            paypalParameters.AppendFormat("&amount={0}", "12.35");
+            paypalParameters.AppendFormat("&item_name={0}", "Mini Red Session");
+            paypalParameters.AppendFormat("&notify_url={0}", "https://api.coachseek.com/PayPal/Notify");
+            paypalParameters.AppendFormat("&return={0}", "https://api.coachseek.com/PayPal/return");
+            paypalParameters.AppendFormat("&invoice={0}", "ABC123");
+
+            var url = String.Format("https://www.sandbox.paypal.com/cgi-bin/webscr?cmd=_xclick&{0}", paypalParameters);
+            return new RedirectResult(url);
         }
     }
 }
