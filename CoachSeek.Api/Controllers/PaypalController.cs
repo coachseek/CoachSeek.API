e6cdb709f405c658ba89071d336cb5c0e08d0b0b
Olaf Thielke <olaf@coachseek.com>
2015-06-02 13:27:35 +1200

    Intercept PayPal IPN messages.

---
+++ CoachSeek.Api/Controllers/PaypalController.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.Http;
+using System.Web.Http;
+using CoachSeek.Application.Services.Emailing;
+using Coachseek.Integration.Contracts.Models;
+
+namespace CoachSeek.Api.Controllers
+{
+    public class PaypalController : BaseController
+    {
+        [AllowAnonymous]
+        public HttpResponseMessage Post()
+        {
+            var form = Request.Content.ReadAsStringAsync();
+
+            var emailer = EmailerFactory.CreateEmailer(false, Context.EmailContext);
+
+            var email = new Email(EmailSender, "olaf@coachseek.com", "API PayPal IPN test", form.Result);
+            var successful = emailer.Send(email);
+
+            return null;
+        }
+    }
+}
