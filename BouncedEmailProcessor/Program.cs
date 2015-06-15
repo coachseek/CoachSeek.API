1e143708363555ed600c997bafe4806520ceb659
Olaf Thielke <olaf@coachseek.com>
2015-06-15 14:56:30 +1200

    Add WebJob for BouncedEmailProcessor.

--- BouncedEmailProcessor/Program.cs
+++ BouncedEmailProcessor/Program.cs
@@ -1,21 +1,42 @@
-﻿using Coachseek.API.Client.Services;
+﻿using System.Timers;
+using Coachseek.API.Client.Services;
 using CoachSeek.Application.UseCases.Emailing;
 using Coachseek.Infrastructure.Queueing.Amazon;
 
 namespace Coachseek.Integration.Emailing.BouncedEmailProcessor
 {
-    /// <summary>
-    /// Prototype bounced email processor.
-    /// </summary>
     class Program
     {
+        private const int MILLISECONDS_IN_SECOND = 1000;
+
+        static Timer Timer { get; set; }
+
         static void Main(string[] args)
+        {
+            SetTimer();
+            Timer.Start();
+
+            while (true)
+            { }
+        }
+
+
+        private static void TimerElapsed(object sender, ElapsedEventArgs e)
         {
             var queueClient = new AmazonBouncedEmailQueueClient();
             var apiClient = new CoachseekAdminApiClient(new AdminApiClient());
 
             var useCase = new ProcessBouncedEmailMessagesUseCase(queueClient, apiClient);
             useCase.Process();
+
+            SetTimer(60);
+            Timer.Start();
+        }
+
+        private static void SetTimer(int numberOfSeconds = 1)
+        {
+            Timer = new Timer { Interval = numberOfSeconds * MILLISECONDS_IN_SECOND, AutoReset = false };
+            Timer.Elapsed += TimerElapsed;
         }
     }
 }
