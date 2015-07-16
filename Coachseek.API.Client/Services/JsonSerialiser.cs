b60597291a60fce763ba7b23d9aadd13f519a4e9
Olaf Thielke <olaf@coachseek.com>
2015-07-16 18:16:57 +1200

    Create JsonSerialiser class.

---
+++ Coachseek.API.Client/Services/JsonSerialiser.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace Coachseek.API.Client.Services
+{
+    public static class JsonSerialiser
+    {
+        public static string Serialise(object toBeSerialised)
+        {
+            return JsonConvert.SerializeObject(toBeSerialised, Formatting.Indented, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore });
+        }
+    }
+}
