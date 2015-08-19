c76e28229f5a906535c952f5d144eca08e86d1e3
Olaf Thielke <olaf@coachseek.com>
2015-08-19 19:46:19 +1200

    Add IsTesting column to Business table.

--- Coachseek.Repository.SqlServerDatabase/Tables/Business.sql
+++ Coachseek.Repository.SqlServerDatabase/Tables/Business.sql
@@ -6,11 +6,14 @@
     [Currency]                  NCHAR (3)        NOT NULL,
     [PaymentProvider]           NVARCHAR (50)    NULL,
     [MerchantAccountIdentifier] NVARCHAR (100)   NULL,
-	[CreatedOn]					DATETIME2 (7)    CONSTRAINT [DF_Business_CreatedOn]  DEFAULT (getutcdate()) NOT NULL,
+    [CreatedOn]                 DATETIME2 (7)    CONSTRAINT [DF_Business_CreatedOn] DEFAULT (getutcdate()) NOT NULL,
     [IsOnlinePaymentEnabled]    BIT              CONSTRAINT [DF_Business_IsOnlinePaymentEnabled] DEFAULT ((0)) NOT NULL,
     [ForceOnlinePayment]        BIT              CONSTRAINT [DF_Business_ForceOnlinePayment] DEFAULT ((0)) NOT NULL,
+    [IsTesting]                 BIT              CONSTRAINT [DF_Business_IsTesting] DEFAULT ((0)) NOT NULL,
     CONSTRAINT [PK_Business] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
+
+
 
 GO
 CREATE UNIQUE NONCLUSTERED INDEX [UX_Business_Guid]
