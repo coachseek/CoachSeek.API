d0dba26d7f1e029811349f365fdd14ae6b61fae6
Olaf Thielke <olaf@coachseek.com>
2015-10-10 13:45:18 +1300

    Unlink webjobs from API project.

--- BouncedEmailProcessor/Program.cs
+++ BouncedEmailProcessor/Program.cs
@@ -6,35 +6,38 @@ namespace Coachseek.Integration.Emailing.BouncedEmailProcessor
 {
     class Program
     {
-        private const int MILLISECONDS_IN_SECOND = 1000;
+        //private const int MILLISECONDS_IN_SECOND = 1000;
 
-        static Timer Timer { get; set; }
+        //static Timer Timer { get; set; }
 
         static void Main(string[] args)
         {
-            SetTimer();
-            Timer.Start();
-
-            while (true)
-            { }
-        }
-
-
-        private static void TimerElapsed(object sender, ElapsedEventArgs e)
-        {
             var container = new Container(new TypeRegistry());
             var useCase = container.GetInstance<IProcessBouncedEmailMessagesUseCase>();
-
             useCase.Process();
 
-            SetTimer(60);
-            Timer.Start();
+            //SetTimer();
+            //Timer.Start();
+            //while (true)
+            //{ }
         }
 
-        private static void SetTimer(int numberOfSeconds = 1)
-        {
-            Timer = new Timer { Interval = numberOfSeconds * MILLISECONDS_IN_SECOND, AutoReset = false };
-            Timer.Elapsed += TimerElapsed;
-        }
+
+        //private static void TimerElapsed(object sender, ElapsedEventArgs e)
+        //{
+        //    var container = new Container(new TypeRegistry());
+        //    var useCase = container.GetInstance<IProcessBouncedEmailMessagesUseCase>();
+
+        //    useCase.Process();
+
+        //    SetTimer(60);
+        //    Timer.Start();
+        //}
+
+        //private static void SetTimer(int numberOfSeconds = 1)
+        //{
+        //    Timer = new Timer { Interval = numberOfSeconds * MILLISECONDS_IN_SECOND, AutoReset = false };
+        //    Timer.Elapsed += TimerElapsed;
+        //}
     }
 }
--- Coachseek.Integration.Payments.PaymentsProcessor/Program.cs
+++ Coachseek.Integration.Payments.PaymentsProcessor/Program.cs
@@ -8,38 +8,40 @@ namespace Coachseek.Integration.Payments.PaymentsProcessor
 {
     class Program
     {
-        private const int MILLISECONDS_IN_SECOND = 1000;
+        //private const int MILLISECONDS_IN_SECOND = 1000;
 
-        static Timer Timer { get; set; }
+        //static Timer Timer { get; set; }
 
         static void Main(string[] args)
         {
             ApplicationAutoMapperConfigurator.Configure();
             DbAutoMapperConfigurator.Configure();
-
-            SetTimer();
-            Timer.Start();
-
-            while (true)
-            { }
-        }
-
-
-        private static void TimerElapsed(object sender, ElapsedEventArgs e)
-        {
             var container = new Container(new TypeRegistry());
             var useCase = container.GetInstance<IProcessPaymentsUseCase>();
-
             useCase.Process();
 
-            SetTimer(60);
-            Timer.Start();
+            //SetTimer();
+            //Timer.Start();
+            //while (true)
+            //{ }
         }
 
-        private static void SetTimer(int numberOfSeconds = 1)
-        {
-            Timer = new Timer { Interval = numberOfSeconds * MILLISECONDS_IN_SECOND, AutoReset = false };
-            Timer.Elapsed += TimerElapsed;
-        }
+
+        //private static void TimerElapsed(object sender, ElapsedEventArgs e)
+        //{
+        //    var container = new Container(new TypeRegistry());
+        //    var useCase = container.GetInstance<IProcessPaymentsUseCase>();
+
+        //    useCase.Process();
+
+        //    SetTimer(60);
+        //    Timer.Start();
+        //}
+
+        //private static void SetTimer(int numberOfSeconds = 1)
+        //{
+        //    Timer = new Timer { Interval = numberOfSeconds * MILLISECONDS_IN_SECOND, AutoReset = false };
+        //    Timer.Elapsed += TimerElapsed;
+        //}
     }
 }
