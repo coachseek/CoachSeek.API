ad4bd49cbf9e8f7a881dd4917ecdbfc344667596
Olaf Thielke <olaf@coachseek.com>
2015-12-16 16:54:35 +1300

    Remove obsolete CustomerData table.

--- Coachseek.Repository.SqlServerDatabase/Tables/CustomerData.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[CustomerData] (
-    [BusinessId]  INT            NULL,
-    [FirstName]   NVARCHAR (50)  NOT NULL,
-    [LastName]    NVARCHAR (50)  NOT NULL,
-    [Email]       NVARCHAR (100) NULL,
-    [Phone]       NVARCHAR (50)  NULL,
-    [DateOfBirth] DATE           NULL
-);
-
