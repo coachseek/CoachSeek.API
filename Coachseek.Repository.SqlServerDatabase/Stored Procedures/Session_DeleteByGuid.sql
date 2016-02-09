3794779ffae7926d4bfce79f7167e8ea235f0013
Olaf Thielke <olaf@coachseek.com>
2016-02-10 12:29:43 +1300

    Fix courses not deleting when all sessions are deleted.

--- Coachseek.Repository.SqlServerDatabase/Stored Procedures/Session_DeleteByGuid.sql
+++ Coachseek.Repository.SqlServerDatabase/Stored Procedures/Session_DeleteByGuid.sql
@@ -1,4 +1,5 @@
 ﻿
+
 CREATE PROCEDURE [dbo].[Session_DeleteByGuid]
 	@businessGuid uniqueidentifier,
 	@sessionGuid uniqueidentifier
@@ -6,11 +7,22 @@ AS
 BEGIN
 	SET NOCOUNT ON;
 
+	DECLARE @parentId int
+
+	SELECT
+		@parentId = ParentId
+	FROM
+		[dbo].[Session] s
+		INNER JOIN [dbo].[Business] b
+			ON b.[Id] = s.[BusinessId] 
+	WHERE
+		b.[Guid] = @businessGuid
+		AND s.[Guid] = @sessionGuid;
 
 	WITH Course AS 
 	( 
 		SELECT 
-			s.[Id], 
+			s.[ID], 
 			s.[ParentId]
 		FROM 
 			[dbo].[Session] s
@@ -21,14 +33,14 @@ BEGIN
 			AND b.[Guid] = @businessGuid
 		UNION ALL 
 		SELECT
-			s2.[Id], 
+			s2.[ID], 
 			s2.[ParentId]
 		FROM
 			[Session] s2
 			INNER JOIN Course 
-				ON Course.[Id] = s2.[ParentId] 
+				ON Course.[ID] = s2.[ParentId] 
 	)
-	
+
 	DELETE 
 		s
 	FROM 
@@ -36,5 +48,26 @@ BEGIN
 		INNER JOIN Course c
 			ON s.[Id] = c.[Id]
 
+	IF @parentId IS NOT NULL -- it's a session
+	BEGIN
+		DECLARE @courseSessionCount int
+
+		SELECT
+			@courseSessionCount = COUNT(*)
+		FROM
+			[dbo].[Session] s
+		WHERE
+			s.[ParentId] = @parentId
+		
+		IF @courseSessionCount = 0 -- no sessions left in course
+		BEGIN
+			DELETE 
+				s
+			FROM 
+				[dbo].[Session] s
+			WHERE 
+				s.[Id] = @parentId
+		END
+	END
 END
 
